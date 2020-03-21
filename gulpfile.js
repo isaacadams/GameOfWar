d503a0f9b53abacdacfd7bd2a2b38719eb22619c
Isaac Adams <isaac.d.adams@gmail.com>
2020-03-21 14:55:16 -0400

    using parcel to bundle application

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,17 @@
 "use strict";
 
 var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    merge = require('merge-stream'),
-    rimraf = require("rimraf"),
-    fs = require("fs"),
-    browserify = require("browserify"),
-    lessify = require('lessify'),
-    path = require('path');
+    rimraf = require("rimraf");
 
 let myPaths = {
     source: 'app',
-    publish: 'gameofwar'
+    publish: 'dist'
 };
 
-function CreateGameBundle(cb) {
-    let { createFile } = require('@isaacadams/nodejs-utils');
-
-    let bundle = {
-        source: `${myPaths.source}/scripts`,
-        entry: 'GameOfWarPage.jsx',
-        publish: myPaths.publish,
-        module: 'index.js'
-    };
-
-    var customOpts = {
-        entries: [`${bundle.source}/${bundle.entry}`]
-    };
-    var b = browserify(customOpts);
-
-    b.transform("babelify", { presets: ['env', 'react'] });
-    b.transform(lessify);
-
-    b.bundle()
-        .pipe(createFile(`${bundle.publish}/${bundle.module}`));
-
-    return cb();
-}
-
-function MoveCardImages() {
-    return gulp.src(`${myPaths.source}/images/playingcards/1x/**/*.png`)
+function MoveCardImages(cb) {
+    gulp.src(`${myPaths.source}/images/playingcards/1x/**/*.png`)
         .pipe(gulp.dest(`${myPaths.publish}/playingcards`));
-}
-
-function createIndexHtmlFile(cb) {
-
-    let create = fs.createWriteStream(`${myPaths.publish}/index.html`);
-
-    create.write(
-        `<html>
-<body>    
-    <div id="import_gameofwar"></div>
-</body>
-</html>
-
-<footer>
-    <script src="index.js"></script>
-</footer>`
-    );
-
-    create.close();
-
+    
     return cb();
 }
 
@@ -69,9 +20,5 @@ gulp.task('app.clean', function (cb) {
 });
 
 gulp.task('app.images', MoveCardImages);
-gulp.task('app.bundle', CreateGameBundle);
-gulp.task('app.index', createIndexHtmlFile);
 
-gulp.task('app.build', gulp.parallel('app.images', 'app.bundle'));
-
-gulp.task('app', gulp.series('app.clean', 'app.build', 'app.index'));
+gulp.task('app', gulp.series('app.clean', 'app.images'));
