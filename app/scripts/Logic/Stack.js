b49a303323daa6d4b28505dffcb7bb3da0783f9e
Isaac Adams <isaac.d.adams@gmail.com>
2020-03-21 15:48:45 -0400

    adding more comments about how stack is used in game of war

--- app/scripts/Logic/Stack.js
+++ app/scripts/Logic/Stack.js
@@ -1,6 +1,9 @@
 /**
  * Stacks follow the LIFO: Last In, First Out
  * Any element that is added into the array will be the first returned from it
+ * 
+ * In Game of War, the concept of a stack is used when a 'War' event occurs
+ * the card on the top of their stack was the last on in and will be the first one to be taken out into their hand if they win or, if not, the other player's hand
  */
 export class Stack {
     constructor() {
