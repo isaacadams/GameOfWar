6d19e7b7c6ecb51c8a020737254e41ed5e90d77f
Isaac Adams <isaac.d.adams@gmail.com>
2020-03-21 15:35:44 -0400

    spacing methods out

--- app/scripts/Logic/Player.js
+++ app/scripts/Logic/Player.js
@@ -1,22 +1,27 @@
 import { Card } from './Card';
 import { Queue } from './Queue';
 var util = require('util');
+
 export class Player {
     constructor(isComputer) {
         this.isComputer = isComputer;
         this.name = this.isComputer ? 'Computer' : 'User';
+
         this.hand = new Queue();
         this.stack = [];
         this.viewStack = [];
     }
+    
     add(card) {
         this.hand.enqueue(card);
     }
+    
     draw(numberOfCardsToDraw) {
         for (var i = 0; i < numberOfCardsToDraw; i++) {
             this.stack.push(this.hand.dequeue());
         }
     }
+    
     transferStack(queue) {
         this.viewStack = [];
         var length = this.stack.length;
@@ -26,6 +31,7 @@ export class Player {
             queue.enqueue(card);
         }
     }
+    
     get cardInPlay() {
         return util.isNullOrUndefined(this.stack[0]) ? new Card() : this.stack[this.stack.length - 1];
     }
