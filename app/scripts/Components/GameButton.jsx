450c46888deecf59b5748c8b32f067c382135443
Isaac Adams <isaac.d.adams@gmail.com>
2020-03-24 00:31:10 -0400

    using function syntax

--- app/scripts/Components/GameButton.jsx
+++ app/scripts/Components/GameButton.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export const GameButton = (props) => {
+export function GameButton (props) {
     return (
         <button onClick={props.onClick} className="btn">
             {props.content}
