1984479ba033e05e7069be7924708481585dbbab
Isaac Adams <isaac.d.adams@gmail.com>
2020-08-15 20:55:35 -0400

    changed from jsx to tsx

---
+++ src/scripts/GameOfWarPage.tsx
@@ -0,0 +1,32 @@
+require('../styles/game.less');
+import * as React from 'react';
+var ReactDOM = require('react-dom');
+
+import { GameOfWar } from './Logic/GameOfWar';
+var { GameButton } = require('./Components/GameButton.jsx');
+var { GamePlayer } = require('./Components/GamePlayer.jsx');
+
+const game = new GameOfWar();
+function GameOfWarPage() {
+    let [gameState, setState] = React.useState(game.state);
+
+    return(
+        <div id="GameOfWar">
+            <div className="panel">
+                <GameButton content={gameState.buttonMessage} onClick={e => handleClick(e)} />
+                <div className="status">{gameState.roundMessage}</div>
+            </div>
+            <div className="panel">
+                {game.players.map((p, i) => <GamePlayer player={p} key={i} />)}
+            </div>
+        </div>
+    );
+
+    function handleClick(event) {
+        if(gameState.gameOver) return;
+        game.nextRound();
+        setState(game.state);
+    }
+}
+
+ReactDOM.render(<GameOfWarPage />, document.getElementById('import_gameofwar'));
