9d7913c6732c87779599f2dbb6466c94cb4dbde7
Isaac Adams <isaac.d.adams@gmail.com>
2021-02-27 19:10:18 -0500

    added babel packages and config for transpiling test modules

--- tests/init.test.js
+++ tests/init.test.js
@@ -1,28 +1,27 @@
 //import { getRandomInt } from '@isaacadams/extensions';
-import { Card } from '../app/scripts/Logic/Card';
+import { Card } from '../src/scripts/Logic/Card';
 
 // https://jestjs.io/docs/en/api
 
-
 expect.extend({
-    toBeWithinRange(received, floor, ceiling) {
-        const pass = received >= floor && received <= ceiling;
-        if (pass) {
-            return {
-                message: () =>
-                `expected ${received} not to be within range ${floor} - ${ceiling}`,
-                pass: true,
-            };
-        } else {
-            return {
-                message: () =>
-                `expected ${received} to be within range ${floor} - ${ceiling}`,
-                pass: false,
-            };
-        }
+  toBeWithinRange(received, floor, ceiling) {
+    const pass = received >= floor && received <= ceiling;
+    if (pass) {
+      return {
+        message: () =>
+          `expected ${received} not to be within range ${floor} - ${ceiling}`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () =>
+          `expected ${received} to be within range ${floor} - ${ceiling}`,
+        pass: false,
+      };
     }
+  },
 });
 
 test('new card has a value of 0', () => {
-    expect(new Card().value).toEqual(undefined);
+  expect(new Card().value).toEqual(undefined);
 });
